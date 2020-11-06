66d0833c9370722b8fb34e8a386559e5a3f92fad
Bishal Choudhary <bishalchoudhary67@gmail.com>
2020-11-06 22:13:57 +0530

    changes done

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
       <div id="root">
-        <p>"I am learning React. My life is getting better. "</p>
+        <p>I am learning React. My life is getting better.</p>
       </div>
     </>
   );
